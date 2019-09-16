0bfcdca0502e1a3cb5fb049063af2beef44c2260
xiyuliu19 <xiyuliu@usc.edu>
2019-09-16 00:11:56 -0700

    added help

---
+++ query_server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+usage()
+{
+
+cat <<EOF
+Usage: $0 [OPTION]
+
+Prints the name of the current server as: "Current Server:[servername]
+
+   -l  Prints the username with the servername
+
+Exit status:
+ 0  if OK,
+ 1  Problem with command line
+EOF
+}
+if [ ! -n "$1" ]; then
+       echo "Current Server: xiyuliu@trgn.usc.edu!"
+fi
+case $1 in
+        --help) usage; exit 0;;
+        -l) echo "Current Server: xiyuliu@trgn.usc.edu!"; exit 0 ;;
+        *) exit 1 ;;
+esac
